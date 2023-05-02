d6f1cc3c820998fa5abd020ff18a337c2c21594a
xinyizhiren <3081014477@qq.com>
2023-05-02 17:54:41 +0800

    音频可视化

--- 音频示波器/Form1.Designer.cs
+++ 音频示波器/Form1.Designer.cs
@@ -34,10 +34,16 @@ namespace 音频示波器
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.exit = new System.Windows.Forms.ToolStripTextBox();
+            this.color = new System.Windows.Forms.ToolStripComboBox();
+            this.is_top = new System.Windows.Forms.ToolStripComboBox();
+            this.is_fixed = new System.Windows.Forms.ToolStripComboBox();
+            this.size = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
             this.opacity = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exit = new System.Windows.Forms.ToolStripTextBox();
+            //this.Cursor = Cursors.Hand;
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,26 +58,74 @@ namespace 音频示波器
             // 
             this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.is_top,
+            this.is_fixed,
+            this.size,
+            this.color,
             this.opacity,
             this.toolStripSeparator1,
             this.exit});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(301, 134);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(301, 313);
             // 
-            // toolStripSeparator1
+            // color
             // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(297, 6);
+            this.color.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.color.Items.AddRange(new object[] {
+            "炫酷蓝色",
+            "靛青色",
+            "青绿色",
+            "碧绿色",
+            "酸橙色",
+            "银色",
+            "暗灰色",
+            "深青色",
+            "紫罗兰色"});
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(121, 39);
             // 
-            // exit
+            // is_top
             // 
-            this.exit.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.exit.Name = "exit";
-            this.exit.ReadOnly = true;
-            this.exit.Size = new System.Drawing.Size(100, 38);
-            this.exit.Text = "退出";
-            this.exit.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.exit.Click += new System.EventHandler(this.exit_Click);
+            this.is_top.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.is_top.Items.AddRange(new object[] {
+            "Top",
+            "Bottom"});
+            this.is_top.Name = "is_top";
+            this.is_top.Size = new System.Drawing.Size(121, 39);
+            this.is_top.SelectedIndexChanged += new System.EventHandler(this.is_top_SelectedIndexChanged);
+            // 
+            // is_fixed
+            // 
+            this.is_fixed.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.is_fixed.Items.AddRange(new object[] {
+            "固定",
+            "不固定"});
+            this.is_fixed.Name = "is_fixed";
+            this.is_fixed.Size = new System.Drawing.Size(121, 39);
+            this.is_fixed.SelectedIndexChanged += new System.EventHandler(this.is_fixed_SelectedIndexChanged);
+            // 
+            // size
+            // 
+            this.size.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.size.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox2});
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(300, 38);
+            this.size.Text = "大小（宽度）";
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Items.AddRange(new object[] {
+            1920,
+            1680,
+            1500,
+            1200,
+            1000,
+            800,
+            400});
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 39);
+            this.toolStripComboBox2.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox2_SelectedIndexChanged);
             // 
             // opacity
             // 
@@ -95,11 +149,27 @@ namespace 音频示波器
             this.toolStripComboBox1.Size = new System.Drawing.Size(121, 39);
             this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(297, 6);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.exit.Name = "exit";
+            this.exit.ReadOnly = true;
+            this.exit.Size = new System.Drawing.Size(100, 38);
+            this.exit.Text = "退出";
+            this.exit.Control.Cursor = Cursors.Default;
+            this.exit.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1068, 874);
+            this.ClientSize = new System.Drawing.Size(1000, 1000);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -120,5 +190,10 @@ namespace 音频示波器
         private ToolStripSeparator toolStripSeparator1;
         private ToolStripMenuItem opacity;
         private ToolStripComboBox toolStripComboBox1;
+        private ToolStripMenuItem size;
+        private ToolStripComboBox toolStripComboBox2;
+        private ToolStripComboBox is_fixed;
+        private ToolStripComboBox is_top;
+        private ToolStripComboBox color;
     }
 }
--- 音频示波器/Form1.cs
+++ 音频示波器/Form1.cs
@@ -2,28 +2,81 @@ using NAudio;
 using NAudio.Dsp;
 using NAudio.Wave;
 using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Net.Http;
 using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+/*
+炫酷蓝色：#00BFFF（0, 191, 255）
+靛青色：#008B8B（0, 139, 139）
+青绿色：#00CED1（0, 206, 209）
+碧绿色：#7FFFD4（127, 255, 212）
+酸橙色：#00FF7F（0, 255, 127）
+银色：#C0C0C0（192, 192, 192）
+暗灰色：#A9A9A9（169, 169, 169）
+深青色：#008080（0, 128, 128）
+紫罗兰色：#9400D3（148, 0, 211）
+ */
 
 namespace 音频示波器
 {
     public partial class Form1 : Form
     {
+        // 用于处理WM_NCHITTEST消息的常量
+        private const int WM_NCHITTEST = 0x0084;
+        private const int HTCLIENT = 1;
+        private const int HTCAPTION = 2;
+
+        // 用于移动窗口的常量和方法
+        private const int WM_SYSCOMMAND = 0x0112;
+        private const int SC_MOVE = 0xF010;
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
         WasapiLoopbackCapture cap;
+        Rectangle screenBounds;
         double[] finalData;   //最终数据
         double[] oldData;  //上一次的数据
-
+        bool removed = false;
+        Color[] colors = { Color.FromArgb(0, 191, 255), Color.FromArgb(0, 139, 139) , Color.FromArgb(0, 206, 209) ,
+            Color.FromArgb(127, 255, 212), Color.FromArgb(0, 255, 127), Color.FromArgb(192, 192, 192),
+            Color.FromArgb(169, 169, 169), Color.FromArgb(0, 128, 128), Color.FromArgb(148, 0, 211)};
+        //Color selected_Color;        
+        ColorBlend colorBlend;
+        //可以改一下根据当前位置改变大小
     public Form1()
         {
             InitializeComponent();
-            Rectangle screenBounds = Screen.GetWorkingArea(this);
+            init();
+        }
+        private void init()
+        {
+            //窗口显示位置初始化
+            screenBounds = Screen.GetWorkingArea(this);
             this.ShowInTaskbar = false;
-            // 计算窗口的左上角坐标，使其位于屏幕中心
-            int x = screenBounds.Width - this.Width ;
-            int y = screenBounds.Height - this.Height;
-            this.Location = new Point(x, y);
-            toolStripComboBox1.SelectedIndex = 0;
+            this.Location = new Point(screenBounds.Width - 1000, screenBounds.Height - 1000);
+            //控件初始化
+            using(StreamReader reader = new StreamReader("C:\\data.txt"))
+            {
+                toolStripComboBox1.SelectedIndex = int.Parse(reader.ReadLine());
+                toolStripComboBox2.SelectedIndex = int.Parse(reader.ReadLine());
+                color.SelectedIndex = int.Parse(reader.ReadLine());
+                this.Location = new Point(int.Parse(reader.ReadLine()), int.Parse(reader.ReadLine()));
+            }
+            //toolStripComboBox1.SelectedIndex = 0;
+            //toolStripComboBox2.SelectedItem = 1000;
+            is_fixed.SelectedIndex = 0;
+            is_top.SelectedIndex = 0;
+            //color.SelectedIndex = 0;
+            //笔刷初始化
+            //selected_Color = color1;
+            //colorBlend = new ColorBlend();
+            //colorBlend.Positions = new float[] { 0f, 1f };
+            //colorBlend.Colors = colors;
             Audio_Capture();
         }
         private void Audio_Capture()
@@ -89,13 +142,113 @@ namespace 音频示波器
                 .Select(i => oldData[i] + 0.8*(finalData[i] - oldData[i]))
                 .ToArray();
             }
-            RectangleF[] rectangle = finalData
+            RectangleF[] rectangles = finalData
                 .Select((v, i) => new RectangleF(i * (this.Width / finalData.Length), (float)0.9 * this.Height - (float)(50 * Math.Sqrt(v)), (float)0.6*(this.Width / finalData.Length), (float)(50 * Math.Sqrt(v))))
                 .ToArray();
             //绘图
             Graphics g = e.Graphics;
-            Brush brush = new SolidBrush(Color.FromArgb(0, 191,255));
-            /*
+            Brush brush = new SolidBrush(colors[color.SelectedIndex]);
+            g.FillRectangles(brush, rectangles);
+            oldData = new double[finalData.Length];
+            Array.Copy(finalData, oldData, finalData.Length);
+        }
+
+        //窗口可移动方法实现
+        protected override void WndProc(ref Message m)
+        {
+            // 处理WM_NCHITTEST消息，以实现窗口的移动
+            if (m.Msg == WM_NCHITTEST && removed)
+            {
+                base.WndProc(ref m);
+                if (m.Result == (IntPtr)HTCLIENT)
+                {
+                    m.Result = (IntPtr)HTCAPTION;
+                }
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            // 在鼠标按下时，调用ReleaseCapture和SendMessage方法，移动窗口
+            base.OnMouseDown(e);
+            if (removed && e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE, 0);
+            }
+        }
+        //SelectedIndexChanged事件
+        private void is_fixed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(is_fixed .SelectedIndex == 0)
+            {
+                removed = false;
+            }
+            else
+            {
+                removed = true;
+            }
+        }
+
+        private void is_top_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (is_top.SelectedIndex == 0) this.TopMost = true;
+            else this.TopMost = false;
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Opacity = 1 - toolStripComboBox1.SelectedIndex * 0.25;
+        }
+        private void toolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //1500;1200;1000;800;600;400;200
+            int width = 1000;
+            if (toolStripComboBox2.SelectedItem != null)
+            {
+                width = Convert.ToInt32(toolStripComboBox2.SelectedItem);
+            }
+            int x = this.Location.X + this.Size.Width - width;
+            int y = this.Location.Y + this.Size.Height - width;
+            this.Location = new Point(x, y);
+            this.Size = new Size(width, width);
+        }
+        //退出
+        private void exit_Click(object sender, EventArgs e)
+        {
+            cap.StopRecording();
+            using (StreamWriter writer = new StreamWriter("C:\\data.txt"))
+            {
+                writer.WriteLine(toolStripComboBox1.SelectedIndex);
+                writer.WriteLine(toolStripComboBox2.SelectedIndex);
+                writer.WriteLine(color.SelectedIndex);
+                writer.WriteLine(this.Location.X);
+                writer.WriteLine(this.Location.Y);
+            }
+            this.Close();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//一下为各种版本渐变笔刷尝试
+/*
              * foreach (RectangleF rec in rectangle)
             {
                 //下面这条经常报错
@@ -106,21 +259,40 @@ namespace 音频示波器
                 }
                 
             }
-             */
-            
-            g.FillRectangles(brush, rectangle);
-            oldData = new double[finalData.Length];
-            Array.Copy(finalData, oldData, finalData.Length);
-        }
+             
+            using (var brush = new LinearGradientBrush(new PointF(0, 0), new PointF(0, 110), Color.White, Color.White))
+            {
+                var colorBlend = new ColorBlend();
+                colorBlend.Positions = new float[] { 0f, 1f };
+                colorBlend.Colors = colors;
 
-        private void exit_Click(object sender, EventArgs e)
-        {
-            System.Environment.Exit(0);
-        }
+                brush.InterpolationColors = colorBlend;
 
-        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+                using (var g = Graphics.FromHwnd(this.Handle))
+                {
+                    foreach (var rect in rectangles)
+                    {
+                        g.FillRectangle(brush, rect);
+                    }
+                    //g.FillRectangles(brush, rectangles);
+                }
+            }
+            */
+/*
+using (var g = Graphics.FromHwnd(this.Handle))
+{
+
+    foreach (var rect in rectangles)
+    {
+        if(rect.Height > 0)
         {
-            this.Opacity = 1 - toolStripComboBox1.SelectedIndex * 0.25;
+            //每当音频暂停时将会报错,只能是最后一组数据操作，因为不播放时不会运行该程序
+            using (var brush = new LinearGradientBrush(rect, Color.White, Color.White, LinearGradientMode.Vertical))
+            {
+                brush.InterpolationColors = colorBlend;
+                g.FillRectangle(brush, rect);
+            }
         }
     }
 }
+*/
